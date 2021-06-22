8a988a1bf61928d743bd1f827a4868a75311daa2
alrinajabin <alrinajabin@gmail.com>
2021-06-22 10:48:39 +0530

    latest

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,2 @@
 
 
-const constants = {
-  app: {
-    baseURL: "https://api.vruksha.alpha-03.trizentinc.com",
-    renderFile: "https://api.vruksha.alpha-03.trizentinc.com/files/download",
-  },
-};
-
-export { constants };
